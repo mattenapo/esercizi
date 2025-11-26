7f83746315aa7843e7896ff78839f01463760062
matte <matteo.napoletano@fermi.mo.it>
2025-11-26 20:32:41 +0100

    creazione classe PublisherHandler

--- API_Publisher.py
+++ API_Publisher.py
@@ -8,3 +8,5 @@ client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["publisher_db"]
 publishers_collection = db["publishers"]
 books_collection = db["books"]
+
+class PublisherHandler(tornado.web.RequestHandler):
